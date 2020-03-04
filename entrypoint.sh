349fa6805e6783e38bd49d9372823b0acb449f65
Ben F <benj-fletch@users.noreply.github.com>
2020-03-04 15:06:53 +0000

    DW-3435 - Fix entrypoint (#19)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 
 /usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /etc/jupyterhub/conf/key.pem -out /etc/jupyterhub/conf/cert.pem -days 3650 -nodes -subj '/CN=jupyter'
 
-/usr/local/bin/jupyterhub $@
+jupyterhub $@
