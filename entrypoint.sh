89c11ec1f4abd1f5ef15aeb364c7f75e451c313d
MattToczek <toczek.matt@gmail.com>
2020-05-26 16:52:14 +0100

    reducing selfsigned cert. length to 30 days

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 ## Script to generate SSL certificates and then start jupyterhub
 
-/usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /etc/jupyterhub/conf/key.pem -out /etc/jupyterhub/conf/cert.pem -days 3650 -nodes -subj '/CN=jupyter'
+/usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /etc/jupyterhub/conf/key.pem -out /etc/jupyterhub/conf/cert.pem -days 30 -nodes -subj '/CN=jupyter'
 
 echo "Adding ${USER}"
 adduser -h "/home/${USER}" -D ${USER}
