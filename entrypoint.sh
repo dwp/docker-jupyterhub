fb3793cc36a81280c608a4418e53e96e7085c23c
MattToczek <52243130+MattToczek@users.noreply.github.com>
2020-10-08 16:55:48 +0100

    entrypoint.sh to use bash to get around terminal wrap issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@
 /usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /etc/jupyterhub/conf/key.pem -out /etc/jupyterhub/conf/cert.pem -days 30 -nodes -subj '/CN=jupyter'
 
 echo "Adding ${USER}"
-adduser -h "/home/${USER}" -D --uid 1001 ${USER}
+adduser -h "/home/${USER}" -D --uid 1001 -s /bin/bash ${USER}
 sed -i "s/USERNAME_TO_REPLACE/${USER}/g" /home/${USER}/.sparkmagic/config.json
 sed -i "s/EMR_HOST_NAME_TO_REPLACE/${EMR_HOST_NAME}/g" /home/${USER}/.sparkmagic/config.json
 sed -i "s/LIVY_SESSION_STARTUP_TIMEOUT_SECONDS_TO_REPLACE/${LIVY_SESSION_STARTUP_TIMEOUT_SECONDS:-120}/g" /home/${USER}/.sparkmagic/config.json
