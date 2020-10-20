e66ae5b36d384968b994a0464dbe9a52c7101f69
ian.utley <ian.utley@infinityworks.com>
2020-10-20 14:27:24 +0100

    Map container home to S3 home

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
-
 ## Script to generate SSL certificates and then start jupyterhub
 
 /usr/bin/openssl req -x509 -newkey rsa:4096 -keyout /etc/jupyterhub/conf/key.pem -out /etc/jupyterhub/conf/cert.pem -days 30 -nodes -subj '/CN=jupyter'
 
 echo "Adding ${USER}"
-adduser -h "/home/${USER}" -D --uid 1001 -s /bin/bash ${USER}
+if [ -d /mnt/s3fs/s3-home ]
+then
+    adduser --no-create-home --home "/home/${USER}" -D --uid 1001 -s /bin/bash ${USER}
+    ln -s /mnt/s3fs/s3-home /home/${USER}
+    for f in `ls -1A /etc/skel`
+    do
+      cp -Rf /etc/skel/$f /home/${USER}
+      chown -R "${USER}:${USER}" /home/$USER/$f
+    done
+else
+    adduser --home "/home/${USER}" -D --uid 1001 -s /bin/bash ${USER}
+fi
 sed -i "s/USERNAME_TO_REPLACE/${USER}/g" /home/${USER}/.sparkmagic/config.json
 sed -i "s/EMR_HOST_NAME_TO_REPLACE/${EMR_HOST_NAME}/g" /home/${USER}/.sparkmagic/config.json
 sed -i "s/LIVY_SESSION_STARTUP_TIMEOUT_SECONDS_TO_REPLACE/${LIVY_SESSION_STARTUP_TIMEOUT_SECONDS:-120}/g" /home/${USER}/.sparkmagic/config.json
